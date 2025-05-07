8813c02e07182a1caac4446ce00b86839189bc12
Eric Rizzi <eric.rizzi@gmail.com>
2025-05-07 08:32:20 -0500

    Cleaner header (no "code editor" while in lesson)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,15 @@
 // src/components/Header.tsx
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import styles from "./Header.module.css";
 import { BASE_PATH } from "../config";
 import LessonNavigation from "./LessonNavigation";
 
 const Header: React.FC = () => {
+  const location = useLocation();
+  // Check if the current path starts with '/lesson/'
+  const isLessonPage = location.pathname.startsWith(`${BASE_PATH}lesson/`);
+
   const getNavLinkClass = ({ isActive }: { isActive: boolean }): string =>
     isActive ? `${styles.navLink} ${styles.navLinkActive}` : styles.navLink;
 
@@ -28,11 +32,13 @@ const Header: React.FC = () => {
                 Learning Paths
               </NavLink>
             </li>
-            <li>
-              <NavLink to={`${BASE_PATH}editor`} className={getNavLinkClass}>
-                Code Editor
-              </NavLink>
-            </li>
+            {!isLessonPage && (
+              <li>
+                <NavLink to={`${BASE_PATH}editor`} className={getNavLinkClass}>
+                  Code Editor
+                </NavLink>
+              </li>
+            )}
             <li>
               <NavLink
                 to={`${BASE_PATH}learning-entries`}
