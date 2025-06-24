225b7e82af7f9da658bebbad92bf712d61ea9975
Eric Rizzi <eric.rizzi@gmail.com>
2025-06-24 08:29:48 -0500

    Fix some types in function lesson

--- src/assets/data/00_intro/lesson_05.ts
+++ src/assets/data/00_intro/lesson_05.ts
@@ -14,13 +14,13 @@ const lessonData: Lesson = {
   title: "Introduction to Functions",
   guid: "3ad2a551-618e-4398-918a-02342f824ab1" as LessonId,
   description:
-    "Learn how to create reusable blocks of code with functions and parameters",
+    "Learn how to create reusable blocks of code with functions to simplify your programs",
   sections: [
     {
       kind: "Information",
       id: "functions-intro",
       title: "Welcome to Functions!",
-      content: `So far, you've been writing programs that run from top to bottom, line by line. That's great, but what if you need to do the same thing multiple times? What if you want to organize your code in a better way?\n\nThat's where **functions** come in!\n\nThink of functions like teaching someone a new skill. Once they learn it, you can just say "do that thing we practiced" instead of explaining it all over again. Functions let us **reuse code** without copying and pasting, **organize** our programs into logical chunks, and **avoid mistakes** by writing tricky code just once. Let's dive in and see how functions can make our lives easier!`,
+      content: `So far, you've been writing programs that run from top to bottom, line by line. That's great, but what if you need to do the same thing multiple times? What if you want to organize your code in a better way?\n\nThat's where **functions** come in! Think of functions like teaching someone a new skill. Once they learn it, you can just say "do that thing we practiced" instead of explaining it all over again.\n\nFunctions let us **reuse code** without copying and pasting, **organize** our programs into logical chunks, and **avoid mistakes** by writing tricky code just once. Let's dive in and see how functions can make our lives easier!`,
     } as InformationSectionData,
     {
       kind: "Observation",
@@ -39,8 +39,8 @@ Run this code and see what happens!`,
     } as ObservationSectionData,
     {
       kind: "MultipleChoice",
-      id: "variable-reassignment",
-      title: "Variable Values",
+      id: "function-parts",
+      title: "Function Parts",
       content:
         'As you can see, a function is basically a small chunk of code that\'s waiting to be called. The function is "defined" with the line `def greet():` and then everything that\'s **indented** under the function is the code to be run once the function is called. Once the function has been defined, it can then be called. Which of the following lines of code "calls" the function?',
       options: [
@@ -57,8 +57,8 @@ Run this code and see what happens!`,
     } as MultipleChoiceSectionData,
     {
       kind: "PRIMM",
-      id: "double-quote-strings" as SectionId,
-      title: "Quoting Someone",
+      id: "primm-greet-function" as SectionId,
+      title: "Multiple Function Calls",
       content:
         "Below is some Python code that **defines** and **calls** the `greet()` function. Predict what you think the code will output, then run it to check your prediction.",
       examples: [
@@ -73,7 +73,7 @@ Run this code and see what happens!`,
     } as PRIMMSectionData,
     {
       kind: "Information",
-      id: "thoughtful-intro",
+      id: "function-summary",
       title: "Function Summary",
       content:
         "There are seemingly a lot of parts to a function, but really it's just four main pieces:\n1. `def` starts defining the function\n2. `greet` is the name of the function\n3. The indented code below the `def greet():` is the code the function will run\n4. The unindented `greet()` **calls** the function\n\nFor more information about functions, watch [this video](https://youtu.be/u-OmVr_fT4s?si=ZLunXpE8Bhk23lsD&t=12) up until 02:24 ",
@@ -102,7 +102,7 @@ Run this code and see what happens!`,
       id: "birthday-practice" as SectionId,
       title: "Challenge: Happy Birthday Song",
       content:
-        "Now it's your turn to work with functions! Create a program that:\n\n1. Calls the existing `verse()` and `chorus()` functions to print out the happy birthday song to Alex.",
+        'Now it\'s your turn to work with functions! Create a program that:\n\n1. Calls the existing `verse()` and `chorus()` functions to "sing" happy birthday to Alex.',
       example: {
         id: "birthday-challenge",
         title: "Implement Your Solution",
