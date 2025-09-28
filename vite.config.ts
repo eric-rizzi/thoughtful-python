ee058ec792a23cfc0bd871f4e15e9bac48f23bcd
Eric Rizzi <erizzi@amazon.com>
2025-09-28 16:08:08 -0500

    Add coverage to project

--- vite.config.ts
+++ vite.config.ts
@@ -41,6 +41,20 @@ export default defineConfig({
     environment: "jsdom",
     setupFiles: "./test-setup.ts",
     exclude: ["node_modules", "dist", "e2e/**"],
+    coverage: {
+      provider: "v8", // or 'istanbul'
+      reporter: ["text", "json", "html"], // Generate multiple report formats
+      reportsDirectory: "./.coverage", // Where to store the reports
+      include: ["src/**/*.{ts,tsx}"], // Files to include in coverage
+      exclude: [
+        "src/assets/**",
+        "src/main.tsx",
+        "src/vite-env.d.ts",
+        "src/types/**",
+        "src/mocks/**",
+        "src/contexts/PyodideContext.tsx", // Exclude files that are hard to test
+      ],
+    },
     css: {
       modules: {
         classNameStrategy: "non-scoped",
