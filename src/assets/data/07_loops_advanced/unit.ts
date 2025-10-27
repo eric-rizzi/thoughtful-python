42b55e3df41bdce588adca5c11a61bd0b95dde3e
Eric Rizzi <erizzi@amazon.com>
2025-10-27 18:28:36 -0500

    Begin work on advanced loops. Move other (maybe deletable) stuff later

--- src/assets/data/07_loops_advanced/unit.ts
+++ src/assets/data/07_loops_advanced/unit.ts
@@ -5,13 +5,8 @@ const unitManifest: UnitManifest = {
   title: "Advanced Loops",
   image: "images/unit_icon_loops_advanced.svg",
   description:
-    "Learn how to use loops in your code to further exploit patterns and create even more interesting shapes.",
-  lessons: [
-    "lessons/00_loops_and_vars",
-    "lessons/01_loops_and_funcs",
-    "lessons/02_loops_wrap_up",
-    "lessons/99_test_visual_turtle",
-  ],
+    "Learn how to iterate through data using loops in order to analyze large datasets.",
+  lessons: ["lessons/00_iteration_loops", "lessons/01_accumulation_intro"],
 };
 
 export default unitManifest;
